9bd414d3d21c8a35038ff787e3e6a79fad7db585
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2022-12-20 03:04:40 -0300

    [ADD] Comienzo del proyecto

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { Canvas } from "@react-three/fiber"
+import { Sky } from "@react-three/drei"
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Miducraft</h1>
+      <Canvas>
+
+      </Canvas>
+    </div>
+  )
+
+}
+
+export default App
